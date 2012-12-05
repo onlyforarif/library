c5f1d3ef93ac817821da8f166f5b8cadf3792861
Arif Khan <arif@NX00151.(none)>
2012-12-05 12:00:06 +0500

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Librarysystem::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.precompile = %w{application.js}
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
